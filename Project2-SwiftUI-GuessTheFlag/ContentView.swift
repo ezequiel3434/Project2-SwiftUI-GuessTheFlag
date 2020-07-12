a3df5d34d2ed84dbd85d34f78abd62defc8a71db
Ezequiel <ezequiel3434@gmail.com>
2020-07-11 21:20:28 -0300

    gradients

--- Project2-SwiftUI-GuessTheFlag/ContentView.swift
+++ Project2-SwiftUI-GuessTheFlag/ContentView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+//        LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
+        
+//        RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+        
+        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+        
     }
 }
 
