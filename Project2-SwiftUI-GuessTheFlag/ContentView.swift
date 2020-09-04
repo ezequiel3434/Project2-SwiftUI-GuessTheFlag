dd36137dcd1caebc366cdb13954eed75549a9cde
ezequiel3434 <ezequiel3434@gmail.com>
2020-09-03 22:33:45 -0300

    When you tap the correct flag, make it spin around 360 degrees on the Y axis. Make the other two buttons fade out to 25% opacity. And if you tap on the wrong flag? Well, that’s down to you – get creative! DOne

--- Project2-SwiftUI-GuessTheFlag/ContentView.swift
+++ Project2-SwiftUI-GuessTheFlag/ContentView.swift
@@ -9,10 +9,10 @@
 import SwiftUI
 
 struct ContentView: View {
-//    @State private var showingAlert = false
+    //    @State private var showingAlert = false
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria",
-        "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+                                    "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     
     @State private var correctAnswer = Int.random(in: 0...2)
     
@@ -20,41 +20,52 @@ struct ContentView: View {
     @State private var score: Int = 0
     @State private var scoreTitle = ""
     @State private var message = ""
-    
+    @State private var animate = false
+    @State private var wrongAnswerOpacity = [0.0,0.0,0.0]
+    struct FlagImage: View {
+        var countryName: String
+        
+        var body: some View {
+            Image(countryName).renderingMode(.original)
+                .clipShape(Capsule())
+                .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                .shadow(color: .black, radius: 2)
+        }
+    }
     
     
     var body: some View {
-//        LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
+        //        LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
         
-//        RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
+        //        RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 20, endRadius: 200)
         
-//        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
-//
-//
-//        Button(action: {
-//            print("Button was tapped")
-//        }) {
-//            HStack(spacing: 10){
-//                Image(systemName: "pencil")
-//                Text("Tap me!")
-//            }
-//
-//        }
-        
-        
+        //        AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+        //
+        //
+        //        Button(action: {
+        //            print("Button was tapped")
+        //        }) {
+        //            HStack(spacing: 10){
+        //                Image(systemName: "pencil")
+        //                Text("Tap me!")
+        //            }
+        //
+        //        }
         
         
-//        Button("Show Alert"){
-//            self.showingAlert = true
-//        }
-//        .alert(isPresented: $showingAlert){
-//        Alert(title: Text("Hello SwiftUI"), message: Text("This is some detail message"), dismissButton: .default(Text("Ok")))
-//        }
+        
+        
+        //        Button("Show Alert"){
+        //            self.showingAlert = true
+        //        }
+        //        .alert(isPresented: $showingAlert){
+        //        Alert(title: Text("Hello SwiftUI"), message: Text("This is some detail message"), dismissButton: .default(Text("Ok")))
+        //        }
         
         ZStack{
-                
+            
             LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
-
+            
             VStack(spacing: 30){
                 
                 
@@ -67,28 +78,47 @@ struct ContentView: View {
                         .fontWeight(.black)
                     
                 }
-                    ForEach(0..<3) { number in
-                        Button(action: {
-                            // flag tapped
-                            
-                            self.flagTapped(number)
-                        }) {
-                            Image(self.countries[number]).renderingMode(.original)
-                            .clipShape(Capsule())
-                                .overlay(Capsule().stroke(Color.black, lineWidth: 1))
-                                .shadow(color: .black, radius: 2)
+                ForEach(0..<3) { flagSelection in
+                    Button(action: {
+                        // flag tapped
+                        
+                        if flagSelection == self.correctAnswer {
+                            withAnimation(.easeOut(duration: 1)) {
+                                self.animate = true
+                            }
+                        } else {
+                            withAnimation(.easeOut(duration: 0.25)) {
+                                self.wrongAnswerOpacity[flagSelection] = 1
+                                
+                            }
                         }
+                        
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            self.flagTapped(flagSelection)
+                        }
+                    }) {
+                        FlagImage(countryName: self.countries[flagSelection])
+                    }
+                    .rotation3DEffect(
+                        flagSelection == self.correctAnswer && self.animate ? .degrees(180) : .degrees(0),
+                        axis: (x:0 , y: 1, z: 0)
+                    )
+                        .opacity(flagSelection != self.correctAnswer && self.animate ? 0.25 : 1)
+                        .overlay(Capsule()
+                            .fill(Color.red)
+                         .opacity(self.wrongAnswerOpacity[flagSelection])
+                    )
                     
                 }
                 
                 Text("Score: \(score)")
-                .foregroundColor(.white)
+                    .foregroundColor(.white)
                 
                 Spacer()
             }
             
         }
-        
+            
         .alert(isPresented: $showingScore) {
             Alert(title: Text(scoreTitle), message: Text(message), dismissButton: .default(Text("Continue"), action: {
                 self.ascQuestion()
@@ -106,7 +136,8 @@ struct ContentView: View {
             message = "That’s the flag of \(countries[number])"
             score -= 1
         }
-        
+        self.wrongAnswerOpacity = [0.0,0.0,0.0]
+        self.animate = false
         showingScore = true
     }
     
